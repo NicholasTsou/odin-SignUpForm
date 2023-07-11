b5989637e43d78ebed4316997211da90d1d38fd9
NicholasTsou <nicholastsoukatos@gmail.com>
2023-07-11 13:16:44 -0400

    Finished

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # odin-SignUpForm
+
+Making a simple Sign-Up Form using HTML/CSS/Javascript with validation<br>
+<br>
+
+Using HTML/CSS to make the front-end and then using Javascript to make simple validation on the form.<br>
+
+Notes:<br>
+Better understanding of javascript and how to use it for validation.
+<br>
+Need to finish responsiveness...
--- js/script.js
+++ js/script.js
@@ -1,21 +1,95 @@
-const  firstName = document.getElementById("first_name");
-const  lastName = document.getElementById("last_name");
-const  email = document.getElementById("email");
-const  phone = document.getElementById("phone_number");
-const  pass =  document.getElementById("password");
-const  conpass = document.getElementById("con_password");
-const  submit = document.getElementById("submit");
-const  all = document.querySelectorAll("input");
+var  firstName = document.getElementById("first_name");
+var  lastName = document.getElementById("last_name");
+var  email = document.getElementById("email");
+var  phone = document.getElementById("phone_number");
+var  pass =  document.getElementById("password");
+var  conpass = document.getElementById("con_password");
+var  submit = document.getElementById("submit");
+var  all = document.querySelectorAll("input");
 
-// firstName.addEventListener('input', checkName);
-// lastName.addEventListener('input', checkName);
-pass.addEventListener('input', checkPass);
-conpass.addEventListener('input', checkPass);
+firstName.addEventListener('input', noNum);
+lastName.addEventListener('input', noNum);
+pass.addEventListener('input', checkPasswords);
+conpass.addEventListener('input', checkPasswords);
+email.addEventListener('input', checkEmail);
 submit.addEventListener('click', checkAll);
 
-function checkName(){
+function noNum(event){
+    
+    const value = event.target.value;
+    
+    const newValue = value.replace(/[^a-zA-Z]/g, "");
+    
+    event.target.value = newValue;
+
+    if(newValue.length >= 3){
+        console.log("yes");
+        event.target.classList.add("valid");
+        event.target.classList.remove("invalid");
+    }else{
+        event.target.classList.remove("valid");
+        event.target.classList.add("invalid");
+        event.target.setCustomValidity("Input too short!");
+    }
 
 }
+
+
+function formatPhoneNumber(input) {
+    var phoneNumber = input.replace(/\D/g, ''); // Remove all non-digit characters from the input
+
+    // Add "1 -" prefix
+    var formattedPhoneNumber = '1-';
+
+    // Remove the prefix from the phone number
+    var phoneNumberWithoutPrefix = phoneNumber.replace(/^1/, '');
+
+    var digitCount = 0;
+    for (var i = 0; i < phoneNumberWithoutPrefix.length; i++) {
+    formattedPhoneNumber += phoneNumberWithoutPrefix[i];
+    digitCount++;
+
+      // Add dashes after the first three digits and the next three digits
+    if ((digitCount === 3 || digitCount === 6) && i !== phoneNumberWithoutPrefix.length - 1) {
+        formattedPhoneNumber += '-';
+    }
+}
+
+    return formattedPhoneNumber;
+}
+
+
+    phone.addEventListener("input", function(event) {
+    var inputValue = event.target.value;
+    var formattedValue = formatPhoneNumber(inputValue);
+    event.target.value = formattedValue;
+
+    if(formattedValue.length <= 13){
+        event.target.classList.remove("valid");
+        event.target.classList.add("invalid");
+        event.target.setCustomValidity("Input too short!");
+
+    }else{
+        event.target.classList.add("valid");
+        event.target.classList.remove("invalid");
+    }
+    });
+
+function checkEmail(event){
+    var inputValue = event.target.value;
+
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputValue)){
+        event.target.classList.add("valid");
+        event.target.classList.remove("invalid");
+    }else{
+        event.target.classList.remove("valid");
+        event.target.classList.add("invalid");
+    }
+    
+    event.target.value = inputValue;
+}
+
+
 
 function checkPass(){
 
@@ -24,12 +98,27 @@ function checkPass(){
         pass.classList.remove("valid");
         conpass.classList.add("invalid");
         conpass.classList.remove("valid");
+    
     }else{
 
         pass.classList.remove("invalid");
         pass.classList.add("valid");
         conpass.classList.remove("invalid");
         conpass.classList.add("valid");
+    
+    }
+}
+
+function checkPasswords(event){
+    let values = event.target.value;
+
+    if(values.length < 6){
+        
+        event.target.classList.add("invalid");
+        console.log("not");
+    }else{
+        console.log("6");
+        checkPass();
     }
 }
 
@@ -39,11 +128,14 @@ function checkAll(){
         if(input.value.length === 0){
             input.classList.remove("valid");
             input.classList.add("invalid");
+            input.setCustomValidity("Input can't be empty!");
         }else{
-            input.classList.add("valid");
-            input.classList.remove("invalid");
+            // input.classList.add("valid");
+            // input.classList.remove("invalid");
+            input.setCustomValidity("");
         }
-    }))
+    }));
 
 }
+
 
